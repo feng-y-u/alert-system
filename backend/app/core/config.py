09fe088fc3c9024ed5386c2e65cd95c9f8b603d1
feng-y-u <1834677757@qq.com>
2026-07-07 15:40:37 +0800

    再次修补

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,4 @@
+from pydantic import ConfigDict
 from pydantic_settings import BaseSettings
 
 
@@ -18,9 +19,7 @@ class Settings(BaseSettings):
     EMAIL_USER: str = ""
     EMAIL_PASSWORD: str = ""
 
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
+    model_config = ConfigDict(env_file=".env", case_sensitive=True)
 
 
 settings = Settings()
