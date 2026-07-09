7dccb819bf3b5071a60e881aa88d0d167247222f
feng-y-u <1834677757@qq.com>
2026-07-09 16:34:10 +0800

    fix: move status column to left, after username

--- frontend/src/components/logs/LogTable.vue
+++ frontend/src/components/logs/LogTable.vue
@@ -1,17 +1,17 @@
 <template>
   <el-table :data="logs" class="log-table">
     <el-table-column prop="username" label="用户名" min-width="120" />
+    <el-table-column prop="login_status" label="状态" width="60" align="left">
+      <template #default="{ row }">
+        <StatusTag :status="row.login_status" />
+      </template>
+    </el-table-column>
     <el-table-column prop="login_time" label="登录时间" min-width="160">
       <template #default="{ row }">
         {{ formatDateTime(row.login_time) }}
       </template>
     </el-table-column>
     <el-table-column prop="ip_address" label="IP地址" min-width="140" />
-    <el-table-column prop="login_status" label="状态" width="60" align="left">
-      <template #default="{ row }">
-        <StatusTag :status="row.login_status" />
-      </template>
-    </el-table-column>
   </el-table>
 </template>
 
